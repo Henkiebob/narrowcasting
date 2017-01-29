3e83b487d4d63a5234ec7fa87a9346e7bca6f65b
Tjerk Dijkstra <tjerk.dijkstra@gmail.com>
2017-01-29 15:56:19 +0100

    added some styling

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -2,8 +2,35 @@
 @extends('layouts/app')
 
 @section('content')
-  @foreach($rooms as $room)
-    hallo
-    {{$room->name}}
-  @endforeach
+  <div class="row">
+    <div class="col-md-2">
+      Sidebar
+    </div>
+    <div class="col-md-10">
+      <div class="panel panel-default">
+         <div class="panel-heading">Kamer overzicht</div>
+          <div class="panel-body">
+            Hier kan nog iets van een beschrijving staan
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Naam</th>
+                <th>Bewerken</th>
+                <th>Verwijderen</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($rooms as $room)
+                <tr>
+                  <td>{{$room->name}}</td>
+                  <td><a href="#">Bewerken</a></td>
+                  <td><a href="#">Verwijderen</a></td>
+                </tr>
+              @endforeach
+              </tbody>
+            </table>
+        </div>
+    </div>
+  </div>
 @endsection
